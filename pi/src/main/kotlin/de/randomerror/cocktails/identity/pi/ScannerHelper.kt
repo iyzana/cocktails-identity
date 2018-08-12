1deda70d62fc93fa8314a428fdbbf30a3354e1f0
succcubbus <jannis.kaiser2@gmail.com>
2018-08-13 01:57:56 +0200

    use special file as alternate keyboard input source

--- pi/src/main/kotlin/de/randomerror/cocktails/identity/pi/ScannerHelper.kt
+++ pi/src/main/kotlin/de/randomerror/cocktails/identity/pi/ScannerHelper.kt
@@ -2,11 +2,31 @@ package de.randomerror.cocktails.identity.pi
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.nio.file.Files.newBufferedReader
+import java.nio.file.Paths
 import kotlin.concurrent.thread
 
 fun onPersonScanned(callback: (String) -> Unit) {
     thread(isDaemon = false) {
         BufferedReader(InputStreamReader(System.`in`)).lines()
             .forEach { callback(it) }
+    }
+
+    // use keyboard file in home directory as alternate input source
+    thread(isDaemon = false) {
+        if ("nux" in System.getProperty("os.name")) {
+            // remove content from keyboard file
+            Runtime.getRuntime().exec("sh", arrayOf("-c", "echo -n > keyboard"))
+        }
+
+        val filePath = Paths.get(System.getProperty("user.home"), "keyboard")
+
+        newBufferedReader(filePath).use { reader ->
+            while (true) {
+                val line = reader.readLine() ?: continue
+
+                callback(line)
+            }
+        }
     }
 }
